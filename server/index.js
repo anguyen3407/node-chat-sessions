9f602a9388ab8fd88fbab4c50afcbd303c1f23da
Annie  Nguyen <annienguyen@Annies-MacBook-Air.local>
2017-11-01 12:03:18 -0700

    send to git

--- server/index.js
+++ server/index.js
@@ -1,11 +1,23 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const mc = require( `${__dirname}/controllers/messages_controller` );
-
+const expressSession= require ('session');
+const createInitialSession = require( `${__dirname}/middlewares/session.js`)
 const app = express();
 
 app.use( bodyParser.json() );
 app.use( express.static( `${__dirname}/../public/build` ) );
+
+app.use( session({
+    secret: '1tS a S3cReT',
+    resave: false,
+    saveUnitialized: false,
+    cookie: { maxAge: 1000 }
+}))
+
+app.use(function(req,res,next) {
+    createInitialSession(req, res, next) 
+});
 
 const messagesBaseUrl = "/api/messages";
 app.post( messagesBaseUrl, mc.create );
